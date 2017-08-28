027d301b594a5aa2f1398edfe90a03444ee9426b
Alexandr <yakovenko5893@gmail.com>
2017-08-28 11:03:11 +0300

    100% worked FB and Google Login

--- NewFLoginWithPod/AppDelegate.swift
+++ NewFLoginWithPod/AppDelegate.swift
@@ -8,17 +8,61 @@
 
 import UIKit
 
+
+// FB 
+import FacebookCore
+import  FacebookLogin
+import  FacebookShare
+
+// Google
+import Google
+import GoogleSignIn
+
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
+   
+    
+    var googleOrFacebookLogin = 2
+    
+    static let sharedInstance: AppDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        
+        
         return true
     }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+        
+        
+        
+        if googleOrFacebookLogin == 1 {
+            return FBSDKApplicationDelegate.sharedInstance().application(app,
+                                                                     open: url,
+                                                                     sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String,
+                                                                     annotation: options[UIApplicationOpenURLOptionsKey.annotation]
+        )
+        } else {
+            return GIDSignIn.sharedInstance().handle(url,
+                                                      sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String,
+                                                               annotation: options[UIApplicationOpenURLOptionsKey.annotation])
+        }
+        
+        
+        
+        
+    }
 
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
@@ -39,6 +83,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    func change(index: Int) {
+        googleOrFacebookLogin = index
     }
 
 
--- NewFLoginWithPod/ViewController.swift
+++ NewFLoginWithPod/ViewController.swift
@@ -8,18 +8,103 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import FacebookLogin
+import FacebookCore
+import FacebookShare
 
+import Google
+import GoogleSignIn
+
+
+
+
+class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
+
+    
+    
+    
+    let appDelegate = AppDelegate.sharedInstance
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Google+
+        
+        
+        
+        
+        var error: NSError?
+        GGLContext.sharedInstance().configureWithError(&error)
+        if error != nil {
+            print(error)
+            return
+        }
+        
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().delegate = self
+        
+        
+        let signButton = GIDSignInButton(frame: CGRect(x: 50, y: 120, width: 150, height: 60))
+        view.addSubview(signButton)
+        
+        //  FB ******
+        
+        // Add a custom login button to your app
+        let myLoginButton = UIButton(type: .custom)
+        myLoginButton.backgroundColor = UIColor.darkGray
+        myLoginButton.frame = CGRect(x: 50, y: 50, width: 150, height: 60)
+        myLoginButton.center = view.center;
+        myLoginButton.setTitle("My Login Button", for: .normal)
+        
+        // Handle clicks on the button
+        myLoginButton.addTarget(self, action: #selector(loginButtonClicked), for: .touchUpInside)
+        
+        // Add the button to the view
+        view.addSubview(myLoginButton)
+        
+        
+       
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Once the button is clicked, show the login dialog
+    @objc func loginButtonClicked() {
+        
+        appDelegate.googleOrFacebookLogin = 1
+        let loginManager = LoginManager()
+        
+        
+        
+        loginManager.logIn([ .publicProfile ], viewController: self) { loginResult in
+            switch loginResult {
+            case .failed(let error):
+                print(error)
+            case .cancelled:
+                print("User cancelled login.")
+            case .success(grantedPermissions: let grantedPermissions , declinedPermissions: let declinedPermissions, token: let accessToken):
+            
+                print("Logged in!")
+                
+                self.goToNext()
+            }
+        }
     }
-
-
+    // google
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        print(user.profile.email)
+        goToNext()
+    }
+    
+    
+    func goToNext() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "NextViewController")
+        navigationController?.pushViewController(vc, animated: true)
+        //present(vc, animated: true, completion: nil)
+    }
+    
+    
 }
-
